34ac1a642b4ab914313fbd7a7946c66ea956916d
Takuro Tsujikawa <40074491+takurot@users.noreply.github.com>
2024-11-03 07:15:47 +0900

    Update DynamicRoyaltyNFTContract.sol

--- DynamicRoyaltyNFTContract.sol
+++ DynamicRoyaltyNFTContract.sol
@@ -1,2 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
+contract DynamicRoyaltyNFT is ERC721, ERC2981 {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function setDefaultRoyalty(address receiver, uint96 feeNumerator) external {
+        _setDefaultRoyalty(receiver, feeNumerator);
+    }
+
+    function setTokenRoyalty(uint256 tokenId, address receiver, uint96 feeNumerator) external {
+        _setTokenRoyalty(tokenId, receiver, feeNumerator);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
